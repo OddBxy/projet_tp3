492937eabd1325cf44dafb7672b19d4d37f4388a
OddBxy <theo.lanier@isen.yncrea.fr>
2026-04-08 15:06:56 +0200

    refactor(FruitMarket.sol) : added comments and refactored some data location accessors

--- tp3_smartContract/contracts/FruitMarket.sol
+++ tp3_smartContract/contracts/FruitMarket.sol
@@ -9,20 +9,21 @@ struct Fruit {
 
 contract FruitMarket {
 
-    mapping (string => Fruit) fruitsCatalog;
-    string[] fruitsAvailableIndices;
+    mapping (string => Fruit) fruitsCatalog;    //catalogue des fruits
+    string[] fruitsAvailableIndices;            //on ne peut pas return de mapping, donc on va utiliser aussi un tableau
 
     event update(Fruit fruit);
-    event fruitBought(address sender, string fruitName, uint8 quantity);
-
-    address payable public owner;
-    bool public locked;
+    event fruitBought(address sender, string fruitName, uint8 quantity);    //sert a enregistrer les tx
 
     constructor() payable {
         owner = payable(msg.sender);
     }
 
+
     // -------- mechanismes de securite -------- //
+    address payable public owner;
+    bool public locked;
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can access this function");
         _;
@@ -38,6 +39,8 @@ contract FruitMarket {
     // ----------------------------------------- //
 
 
+    //fonction pour mettre a jour le catalogue
+    //utiliser pour ajouter ou modifier des informations sur un fruit
     function updateCatalog(string calldata name, uint8 price, uint8 quantity) public onlyOwner {
         require(!compareStrings(name, ""), "updateCatalog: fruits must have names");
         require(quantity > 0, "updateCatalog: cannot add 0 fruit");
@@ -45,6 +48,7 @@ contract FruitMarket {
 
         // lorsqu'un element inexistant d'une mapp est indexe, il est initialise avec des valeurs 0 ou "" par defaut
         if(fruitsCatalog[name].quantity == 0){
+            //si c'est le cas, il faut aussi l'ajouter a la liste des references de fruit
             fruitsAvailableIndices.push(name);
         }
 
@@ -56,6 +60,7 @@ contract FruitMarket {
         emit update(fruit);
     }
 
+    //foncton pour supprimer un fruit du catalogue
     function removeFruit(string calldata name) public onlyOwner {
         Fruit storage fruit = fruitsCatalog[name];
         require(fruit.quantity > 0, "removeFruit : fruit not in catalog");
@@ -64,22 +69,26 @@ contract FruitMarket {
     }
 
 
-    function getFruit(string memory fruitName) public view returns (Fruit memory){
+    //fonction pour recuperer un fruit et ses informations
+    function getFruit(string calldata fruitName) public view returns (Fruit memory){
         return fruitsCatalog[fruitName];
     }
 
+
+    //fonction permettant au Dapps de decouvrir les fruits du catalogue
     function getAvailableFruitsList() public view returns (string[] memory) {
         return fruitsAvailableIndices;
     }
 
-
+    //fonction pour acheter un fruit
     function buyFruit(string calldata fruitName, uint8 quantity) external payable {
-        Fruit storage wantedFruit = fruitsCatalog[fruitName];
+        //reference directement les elements du catalogue avec storage et pas memory
+        Fruit storage wantedFruit = fruitsCatalog[fruitName];   
 
         require(wantedFruit.quantity != 0, "buyFruit : fruit requested not available");
         require(wantedFruit.quantity >= quantity, "buyFruit : There is not enough fruit, cannot give you as many as requested");
 
-        //paie les fruits
+        //paiement des fruits
         uint cost = quantity * wantedFruit.price;
         require(msg.value >= cost, "buyFruit : insufficient funds to buy this quantity");
         pay(cost);
@@ -92,12 +101,14 @@ contract FruitMarket {
     }
 
 
+    //fonction de transfer d'ether vers le proprietaire du contrat
     function pay(uint256 _amount) public {
         (bool success,) = owner.call{value: _amount}("");
         require(success, "Failed to send Ether");
     }
 
 
+    //fonction utilitaire pour verifier l'egalite de string
     function compareStrings(string memory a, string memory b) private pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
